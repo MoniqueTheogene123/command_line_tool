560346310a1b417995ad5717b0bc0a8cb57af08d
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-06-16 23:11:14 +0000

    ugh

--- app.rb
+++ app.rb
@@ -5,16 +5,16 @@ require_relative "task"
 person = Person.new(@f_name, @l_name)
 task = Task.new(@detail, @created_at)
 
-print "whats your first name?: "
-person.f_name = gets.chomp
+print "whats your first name:   "
+person.f_name = gets.chomp.capitalize
 
-print "whats your last name?: "
-person.l_name = gets.chomp
+print "whats your last name:   "
+person.l_name = gets.chomp.capitalize
 
-print "please enter your task: "
-task.detail = gets.chomp
+print "please enter your task:  "
+task.detail = gets.chomp.capitalize
 
-puts "\n\n'NewEntry: #{person.full_name} #{task.created_at} #{task.detail}'"
+puts "\n\n'NewEntry: #{person.full_name} #{task.detail}'"
 
 rows = []
 rows << ['person', 'date_series', 'task']
