ea5e1782509a456b41075b45197e78639e05aafb
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-06-16 23:11:23 +0000

    ugh2

--- app.rb
+++ app.rb
@@ -2,23 +2,37 @@ require 'terminal-table'
 require_relative "person"
 require_relative "task"
 
+user_input_all = Hash.new
+
 person = Person.new(@f_name, @l_name)
 task = Task.new(@detail, @created_at)
 
+
 print "whats your first name:   "
-person.f_name = gets.chomp.capitalize
+user_input_all[person.f_name] = gets.chomp.capitalize
 
 print "whats your last name:   "
-person.l_name = gets.chomp.capitalize
+user_input_all[person.l_name] = gets.chomp.capitalize
 
 print "please enter your task:  "
-task.detail = gets.chomp.capitalize
+user_input_all[task.detail] = gets.chomp.capitalize
 
-puts "\n\n'NewEntry: #{person.full_name} #{task.detail}'"
 
-rows = []
-rows << ['person', 'date_series', 'task']
-rows << [person.full_name, task.created_at, task.detail]
-table = Terminal::Table.new :rows => rows
-puts table
+
+# user_input_all["full_name"] = full_name
+# user_input_all["created_at"] = created_at
+# user_input_all["details"] = details
+puts user_input_all
+
+
+
+
+#puts "\n\n'NewEntry: #{person.full_name} #{task.detail}'"
+
+
+# rows = []
+# rows << ['person', 'date_series', 'task']
+# rows << [person.full_name, task.created_at, task.detail]
+# table = Terminal::Table.new :rows => rows
+# puts table
 
